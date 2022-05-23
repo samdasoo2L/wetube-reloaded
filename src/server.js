43c703dd707aa7589d976d81a7621b8934a2f319
Kang MinSeok <alstjr8125@naver.com>
2022-05-23 17:28:40 +0900

    4. Routers

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter.js";
+import videoRouter from "./routers/videoRouter.js";
+import userRouter from "./routers/userRouter.js";
+
+const PORT = 4000;
+
+const app = express();
+const logger = morgan("dev");
+app.use(logger);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+const handleListening = () =>
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
