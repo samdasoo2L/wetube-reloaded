2a43c70f944e1146cb0f55cc54128d55daf0d5a9
Kang MinSeok <alstjr8125@naver.com>
2023-09-29 01:03:58 +0900

    CRUD
    use mongoose, mongoDB, pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,25 +1,52 @@
-import Video from "../models/Video";
+import Video from "../models/Video.js";
 
-export const home = (req, res) => {
-  Video.find({}, (error, videos) => {
-    console.log("errors", error);
-    console.log("videos", videos);
-  });
-  return res.render("home", { pageTitle: "Home", videos: [] });
+export const home = async (req, res) => {
+  // try {
+  //   const videos = await Video.find({});
+  //   return res.render("home", { pageTitle: "Home", videos });
+  // } catch(error) {
+  //   return res.render("server-error", {error});
+  // }
+  const videos = await Video.find({}).sort({ createdAt: "desc" });
+  // console.log(videos);
+  return res.render("home", { pageTitle: "Home", videos });
 };
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params;
+  const video = await Video.findById(id);
+  if (!video) {
+    // 예외처리 (비디오가 없을때!)
+    return res.render("404", { pageTitle: "Video not found." });
+  }
   return res.render("watch", {
-    pageTitle: `Watching`,
+    pageTitle: video.title,
+    video,
   });
 };
-export const getEdit = (req, res) => {
+
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-  return res.render("edit", { pageTitle: `Editing` });
+  const video = await Video.findById(id);
+  if (!video) {
+    // 예외처리 (비디오가 없을때!)
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
-export const postEdit = (req, res) => {
+
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
+  const { title, description, hashtags } = req.body;
+  const video = await Video.exists({ _id: id });
+  if (!video) {
+    // 예외처리 (비디오가 없을때!)
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  await Video.findByIdAndUpdate(id, {
+    title,
+    description,
+    hashtags: Video.formatHashtags(hashtags),
+  });
   return res.redirect(`/videos/${id}`);
 };
 
@@ -27,8 +54,52 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   // here we will add a video to the videos array.
-  const { title } = req.body;
+  const { title, description, hashtags } = req.body;
+  // const video = new Video({
+  //   title,
+  //   description,
+  //   createAt: Date.now(),
+  //   hashtags: hashtags.split(",").map((ward) => `#${ward}`),
+  //   meta: {
+  //     views: 0,
+  //     rating: 0,
+  //   },
+  // });
+  // await video.save();
+  try {
+    await Video.create({
+      title,
+      description,
+      // createAt: Date.now(),
+      hashtags: Video.formatHashtags(hashtags),
+      // meta: {
+      //   views: 0,
+      //   rating: 0,
+      // },
+    });
+    return res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
+};
+
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
   return res.redirect("/");
+};
+
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({ title: keyword });
+  }
+  return res.render("search", { pageTitle: "Search", videos });
 };
--- src/models/Video.js
+++ src/models/Video.js
@@ -1,14 +1,29 @@
 import mongoose from "mongoose";
 
+export const formatHashtags = (hashtags) =>
+  hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`));
+
 const videoSchema = new mongoose.Schema({
-  title: String,
-  description: { type: String },
-  createAt: Date,
+  title: { type: String, required: true, trim: true, maxLength: 80 },
+  description: { type: String, required: true, trim: true, minLength: 20 },
+  createdAt: { type: Date, required: true, default: Date.now },
   hashtags: [{ type: String }],
   meta: {
-    views: Number,
-    rating: Number,
+    views: { type: Number, default: 0, required: true },
+    rating: { type: Number, default: 0, required: true },
   },
+});
+
+// videoSchema.pre("save", async function () {
+//   this.hashtags = this.hashtags[0]
+//     .split(",")
+//     .map((word) => (word.startsWith("#") ? word : `#${word}`));
+// });
+
+videoSchema.static("formatHashtags", function (hashtags) {
+  return hashtags
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word}`));
 });
 
 const Video = mongoose.model("Video", videoSchema);
